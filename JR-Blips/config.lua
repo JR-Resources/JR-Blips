50b9510d1804d30847b0eca2300a5613a70bf433
JR-Resources <150393398+JR-Resources@users.noreply.github.com>
2023-11-09 21:33:06 +0000

    Add files via upload

---
+++ JR-Blips/config.lua
@@ -0,0 +1,27 @@
+Config = Config or {}
+
+-- JR Resources
+  
+Config.blipsShow = true
+
+Config.Locations = {
+-- Example 
+-- [1] = { -- Change to next number in list
+--     vector = vector3(0, 0, 0), -- Blip Location Coords
+--     text = "Test", -- Blip Name
+--     color = 3, -- Blip Color https://docs.fivem.net/docs/game-references/blips/
+--     sprite = 1, -- Blip Sprite https://docs.fivem.net/docs/game-references/blips/
+--     scale = 0.1, -- Blip Size
+-- }, 
+
+-- Add Your Blips Here
+
+[1] = {
+    vector = vector3(538.32, -183.18, 54.47), 
+    text = "Tester", 
+    color = 1, 
+    sprite = 446, 
+    scale = 1.8,
+},
+
+}
---
+++ JR-Blips/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'JR Resources'
+desription 'JR-Blips'
+discord 'https://discord.gg/hCyhRrpMqP'
+version '1.0.0'
+
+client_script 'blips.lua'
+shared_script 'config.lua'
